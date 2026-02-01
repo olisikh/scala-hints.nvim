7c9df7df679ac5bb9b0701caeab2f7a654d4a52b
Oleksii Lisikh <alisiikh@gmail.com>
2026-02-01 01:05:51 +0100

    feat: define virtual lsp capabilities and client metadata

--- lua/scala-hints/constants.lua
+++ lua/scala-hints/constants.lua
@@ -2,4 +2,14 @@ return {
   source = 'scala-hints',
   diagnostic_namespace = 'scala-hints',
   lang = 'scala',
+  client_name = 'scala-hints-lsp',
+  capabilities = {
+    textDocumentSync = {
+      openClose = true,
+      change = 1,
+    },
+    codeActionProvider = {
+      resolveProvider = false,
+    },
+  },
 }
