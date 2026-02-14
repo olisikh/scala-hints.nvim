6f8a73d05b405be8424a2c1f547c9bbae992a181
Oleksii Lisikh <>
2026-02-14 04:37:52 +0100

    add gh actions to run tests on PR

--- lua/scala-hints/libs/kyo/init.lua
+++
@@ -1,9 +0,0 @@
---- Kyo library module for scala-hints (placeholder)
----
---- Will provide Treesitter query definitions for detecting Kyo
---- code patterns and suggesting idiomatic replacements.
-
-return {
-  name = 'kyo',
-  queries = {},
-}
